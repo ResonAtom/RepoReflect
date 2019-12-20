1a333e8e0bc846b85d10b75ef9162cb66496cdac
Adam Thompson <adam@objectivegames.com>
2019-12-20 20:22:36 +0000

    Configure API calls for GithubRepoService, inject service into AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GithubRepoService } from './github-repo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'repo-reflect';
+
+  constructor(private userService: GithubRepoService) { }
+
+  ngOnInit() {
+
+  }
+
 }
--- src/app/github-repo.service.ts
+++ src/app/github-repo.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubRepoService {
 
-  constructor() { }
+  apiUrl = 'https://api.github.com/repos/ResonAtom/RepoReflect/commits';
+
+  constructor(private http: HttpClient) {}
+
+  getCommits() {
+    return this.http.get(`${this.apiUrl}?per_page=100`);
+  }
+
 }
